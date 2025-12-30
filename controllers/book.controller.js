3b8a5c367e8548243b5375c98f2ada936879ae60
ahmadhafifihamdan <ahmadhafifihamdan@gmail.com>
2025-12-30 14:38:21 +0800

    Added book sorting from query

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,7 +1,10 @@
 const db = require("../config/db.config");
 
 const getBooksHandler = async(req, res) => {
-    const [books] = await db.query("SELECT id, BookName, Description, BookImage, BookAuthor FROM book ORDER BY BookName ASC");
+    const { sort } = req.query;
+    const order = String(sort).toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+    const [books] = await db.query(`SELECT id, BookName, Description, BookImage, BookAuthor FROM book ORDER BY BookName ${order}`);
+    
     return res.render("books", { books });
 }
 
